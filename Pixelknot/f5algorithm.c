7e010848b4a9a316a1d7b9f820a7c82b95555417
Gwendolyn Weston <gwensfix@gmail.com>
2017-02-16 12:23:06 -0500

    renaming

--- Pixelknot/f5algorithm.c
+++ Pixelknot/f5algorithm.c
@@ -35,15 +35,15 @@ static void print_string_binary(const char *string) {
 }
 
 static size_t get_message_partition_size(size_t message_size_in_bits, size_t list_size) {
-    size_t message_partition_size = 1;
+    size_t k = 1;
     float embed_rate = (float)message_size_in_bits/list_size;
 
-    while (embed_rate < ((float)message_partition_size/((1<<message_partition_size)-1))){
-        message_partition_size++;
+    while (embed_rate < ((float)k/((1<<k)-1))){
+        k++;
     }
-    message_partition_size -= 2; // just to be safe in case there's lots of zeroes
+    k -= 2; // just to be safe in case there's lots of zeroes
 
-    return message_partition_size;
+    return k;
 }
 
 int just_once = 0;
@@ -53,7 +53,6 @@ static node *fill_coeff_buffer_with_n_coefficients_lsb (uint8_t *lsb_buffer, nod
             remove_from_linked_list(coeff_node);
             coeff_node = coeff_node->next;
         }
-//        if (!just_once) printf("%i ", coeff_node->coeff_struct.coefficient);
 
         lsb_buffer[i] = (coeff_node->coeff_struct.coefficient & 1);
         
@@ -63,11 +62,10 @@ static node *fill_coeff_buffer_with_n_coefficients_lsb (uint8_t *lsb_buffer, nod
         }
         coeff_node = coeff_node->next;
     }
-//    just_once = 1;
     return coeff_node;
 }
 
-static node *fill_buffer (uint8_t *lsb_buffer, node *coeff_node, size_t n, node *array) {
+static node *fill_buffer (uint8_t *lsb_buffer, node *coeff_node, size_t n, short *array) {
     for (size_t i = 0; i < n; i++){
         while (coeff_node->coeff_struct.coefficient==0) {
             remove_from_linked_list(coeff_node);
@@ -75,6 +73,7 @@ static node *fill_buffer (uint8_t *lsb_buffer, node *coeff_node, size_t n, node 
         }
 
         lsb_buffer[i] = (coeff_node->coeff_struct.coefficient & 1);
+        array[i] = coeff_node->coeff_struct.coefficient;
 
         if (coeff_node->next == NULL) {
             printf("sorry, you ran out of coefficients");
@@ -121,13 +120,13 @@ static const char *fill_message_buffer_with_k_message_bits (kbits *const message
     return message;
 }
 
-static int embed_message_bits(size_t index_to_change, size_t codeword_size, node *current_ucb_node) {
+static int embed_message_bits(size_t index_to_change, size_t n, node *current_node) {
 
     if (!index_to_change) {
         return 0; //if zero, there's nothing to embed
     }
 
-    node *node_to_change = traverse_n_nodes_backward(current_ucb_node, codeword_size);
+    node *node_to_change = traverse_n_nodes_backward(current_node, n);
     node_to_change = traverse_n_nodes_forward(node_to_change, index_to_change);
 
     buffer_coefficient *coeff = &node_to_change->coeff_struct;
@@ -137,7 +136,7 @@ static int embed_message_bits(size_t index_to_change, size_t codeword_size, node
         coeff->coefficient++;
 
     if (coeff->coefficient == 0){
-        current_ucb_node = traverse_n_nodes_backward(current_ucb_node, codeword_size);
+        current_node = traverse_n_nodes_backward(current_node, n);
         return 1;
     }
     else {
@@ -145,15 +144,15 @@ static int embed_message_bits(size_t index_to_change, size_t codeword_size, node
     }
 }
 
-int embedMessageIntoCoefficients(const char *message, node *rootOfUsableCoefficientBuffer, size_t list_size){
-    size_t message_partition_size = get_message_partition_size(strlen(message)*8, list_size); //k
-    assert(message_partition_size <= 64);
+int embedMessageIntoCoefficients(const char *message, node *root, size_t list_size){
+    size_t k = get_message_partition_size(strlen(message)*8, list_size); //k
+    assert(k <= 64);
 
-    size_t codeword_size = (1<<message_partition_size)-1; //n
+    size_t n = (1<<k)-1; //n
 
     print_string_binary(message);
 
-    if (message_partition_size < 2){
+    if (k < 2){
         return -1;
     }
     else {
@@ -162,21 +161,18 @@ int embedMessageIntoCoefficients(const char *message, node *rootOfUsableCoeffici
         int shrinkage_flag = 0;
         int current_msgbit_index = 0;
 
-        node *current_ucb_node = rootOfUsableCoefficientBuffer->next;
-        node *debug_node = rootOfUsableCoefficientBuffer->next;
-        node *second_debug_node = rootOfUsableCoefficientBuffer->next;
+        node *current_node = root->next;
+        while (*message != '\0' && current_node) {
 
-        while (*message != '\0' && current_ucb_node) {
+            uint8_t lsb_buffer[n];
 
-            uint8_t lsb_buffer[codeword_size];
-
-            memset(lsb_buffer, 0, codeword_size*sizeof(uint8_t));
-            current_ucb_node = fill_coeff_buffer_with_n_coefficients_lsb(lsb_buffer, current_ucb_node, codeword_size);
-            kbits hash = hash_coefficient_buffer(lsb_buffer, codeword_size);
+            memset(lsb_buffer, 0, n*sizeof(uint8_t));
+            current_node = fill_coeff_buffer_with_n_coefficients_lsb(lsb_buffer, current_node, n);
+            kbits hash = hash_coefficient_buffer(lsb_buffer, n);
 
             //if shrinkage is true, then the correct bits will already be the message_buffer variable
             if (!shrinkage_flag) {
-                message = fill_message_buffer_with_k_message_bits(&message_buffer, message, message_partition_size, &current_msgbit_index);
+                message = fill_message_buffer_with_k_message_bits(&message_buffer, message, k, &current_msgbit_index);
             }
             printf("%llu ", message_buffer);
 
@@ -187,12 +183,12 @@ int embedMessageIntoCoefficients(const char *message, node *rootOfUsableCoeffici
             assert(index_to_change >= 0);
 
 
-            shrinkage_flag = embed_message_bits(index_to_change, codeword_size, current_ucb_node);
+            shrinkage_flag = embed_message_bits(index_to_change, n, current_node);
 
 
         } //end while (*message)
         
-        if (*message != '\0' && current_ucb_node == NULL){
+        if (*message != '\0' && current_node == NULL){
             printf("NOT ENOUGH CAPACITY\n");
             return 0;
         }
@@ -204,26 +200,26 @@ int embedMessageIntoCoefficients(const char *message, node *rootOfUsableCoeffici
         
 }
 
-void extractMessageFromCoefficients(node *rootOfUsableCoefficientBuffer, size_t list_size, size_t output_buffer_size, char *output_buffer) {
-    size_t message_partition_size = get_message_partition_size(output_buffer_size, list_size); // k
+void extractMessageFromCoefficients(node *root, size_t list_size, size_t output_buffer_size, char *output_buffer) {
+    size_t k = get_message_partition_size(output_buffer_size, list_size); // k
     size_t message_size_in_bytes = output_buffer_size/8;
 
     size_t message_index = 0; //how many bytes along have been extracted
     int message_bit_index = 0; //how many bits along (relative to current byte) have been extracted
 
     //calculate the code word length n = 2^k - 1
-    size_t codeword_size = (1<<message_partition_size)-1;  //let this also be known as the variable n
-    node *current_ucb_node = rootOfUsableCoefficientBuffer->next; //mark how far along ucb list we are.
-    uint8_t coeff_buffer[codeword_size]; //buffer to store n bits of codeword
+    size_t n = (1<<k)-1;  //let this also be known as the variable n
+    node *current_node = root->next; //mark how far along ucb list we are.
+    uint8_t coeff_buffer[n]; //buffer to store n bits of codeword
     
     while (message_index < message_size_in_bytes){
         
-        current_ucb_node = fill_coeff_buffer_with_n_coefficients_lsb(coeff_buffer, current_ucb_node, codeword_size);
-        kbits hash = hash_coefficient_buffer(coeff_buffer, codeword_size);
+        current_node = fill_coeff_buffer_with_n_coefficients_lsb(coeff_buffer, current_node, n);
+        kbits hash = hash_coefficient_buffer(coeff_buffer, n);
         printf("%llu ", hash);
 
         //add each bit from k-size hash, starting from the left into current index of extracted message array
-        for (size_t current_hash_bit = 0; current_hash_bit < message_partition_size; current_hash_bit++){
+        for (size_t current_hash_bit = 0; current_hash_bit < k; current_hash_bit++){
             if (message_bit_index > 7){ //we reach end of byte, so increment to next byte of allocated array
                 message_bit_index = 0;
                 message_index++;
@@ -231,7 +227,7 @@ void extractMessageFromCoefficients(node *rootOfUsableCoefficientBuffer, size_t 
             }
             
             //get left-most bit from hash
-            kbits bitmask = 1 << ((kbits) message_partition_size - current_hash_bit - 1);
+            kbits bitmask = 1 << ((kbits) k - current_hash_bit - 1);
             int message_bit = (hash & bitmask) ? 1 : 0;
 
             //add bit to message's current byte
